1ecd6ebd2c63cab1eaf250dc50980b218b1d7842
Marco Jansen <marco.jansen@gmx.us>
2020-12-10 09:57:42 +0800

    javascript functions added for localstorage handling

--- script.js
+++ script.js
@@ -7,6 +7,28 @@ $(document).ready(function() {
     }
     setInterval(update, 1000);
    
- 
+    // Get save button to add input text to local storage
+    $(".savebutton").on("click", function() {
+      
+      var input = $(this).siblings(".form-control").val();
+      var hour = $(this).siblings().attr("id");
+
+      localStorage.setItem(hour, input);
+    });
+
+    // Setting input area for each hour to local storage
+    $("#to-do8").val(localStorage.getItem("hour8"));
+    $("#to-do9").val(localStorage.getItem("hour9"));
+    $("#to-do10").val(localStorage.getItem("hour10"));
+    $("#to-do11").val(localStorage.getItem("hour11"));
+    $("#to-do12").val(localStorage.getItem("hour12"));
+    $("#to-do13").val(localStorage.getItem("hour13"));
+    $("#to-do14").val(localStorage.getItem("hour14"));
+    $("#to-do15").val(localStorage.getItem("hour15"));
+    $("#to-do16").val(localStorage.getItem("hour16"));
+    $("#to-do17").val(localStorage.getItem("hour17"));
+    $("#to-do18").val(localStorage.getItem("hour18"));
   
+  
+
   })
